3ba88360a15f711aab275cad54421e93c26d2f15
Abdessalem MARS <contact@bigda.fr>
2026-07-19 00:09:43 +0200

    Fix: préfixe kafkashaded requis pour le LoginModule JAAS sur Databricks

--- src/databricks_dedup_notebook.py
+++ src/databricks_dedup_notebook.py
@@ -86,7 +86,7 @@ from pyspark.sql.types import (
 )
 
 JAAS_CONFIG = (
-    "org.apache.kafka.common.security.plain.PlainLoginModule required "
+    "kafkashaded.common.security.plain.PlainLoginModule required "
     f'username="{KAFKA_USERNAME}" password="{KAFKA_PASSWORD}";'
 )
 
